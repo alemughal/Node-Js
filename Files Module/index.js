47de83863d5ec03526b59af0fc9f719c4e1cf5a8
alemughal <alimug38@gmail.com>
2021-09-08 18:07:58 +0500

    Reading File with fs module

---
+++ Files Module/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+fs.readFile('test.txt','utf-8',(err,data)=>{
+    if(err) throw err;
+    console.log(data)
+})
+
+const data = fs.readFileSync('test.txt', {encoding:'utf-8',flag: 'r'});
+console.log(data)
+
+fs.stat('test.txt',(err,stats)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(stats.isFile());
+    console.log(stats.isDirectory());
+    console.log(stats.isSymbolicLink());
+    console.log(stats.size);
+    
+
+})
