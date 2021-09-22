8f96ff615d6dc3405483b688da44bdd8a0bbdc91
alemughal <alimug38@gmail.com>
2021-09-22 12:50:47 +0500

    Routing in Node Js

--- Routing/index.js
+++ Routing/index.js
@@ -1,1 +1,59 @@
-console.log("Hello World from Ali Mughal");
+// console.log("Hello World from Ali Mughal");
+
+const http = require("http");
+
+// First Method
+
+// const index = (request, response) => {
+//   response.writeHead(200);
+//   response.end("This is node Application");
+// };
+
+// const about = (request, response) => {
+//   response.end("This is about Page");
+// };
+
+// const services = (request, response) => {
+//   response.end("This is services Page");
+// };
+
+// const contact = (request, response) => {
+//   response.end("This is contact Page");
+// };
+
+// http
+//   .createServer(function (req, res) {
+//     if (req.url == "/") {
+//       return index(req, res);
+//     }
+//   })
+//   .listen(3000);
+
+
+// Another Method
+
+const routes = {
+    "/": function index(request,response) {
+      response.writeHead(200);
+      response.end("This is node Application");
+    },
+    '/about': function about(request,response) {
+      response.end("This is about Page");
+    },
+    '/services': function services(request,response) {
+      response.end("This is services Page");
+    },
+    '/contact': function contact(request,response){
+      response.end("This is contact Page");
+    }
+  
+  };
+
+
+http.createServer(function(req,res){
+    if(req.url in routes){
+        return routes[req.url](req,res);
+    }
+}).listen(3000,()=>{
+    console.log("The serves is listening")
+})
